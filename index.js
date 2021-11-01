d03f640cb3f91ad708467944ac3340b46c52d11e
prayas7102 <prayas/prithvirajpratap7@gmail.com>
2021-11-01 23:46:33 +0530

    joi schema added

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const app=express();
 const path=require('path');
 const methodOverride=require('method-override');
 const ejsmate=require('ejs-mate');
+const joischema =require('./joischema');
+const catchAsync=require('./util/catchAsync');
+const AppError=require('./util/AppError');
 const mongoose=require('mongoose');
 const campground=require('./models/campground');
 mongoose.connect('mongodb://localhost:27017/yelp-camp',{
@@ -18,46 +21,63 @@ app.set('views',path.join(__dirname,'views'));
 app.get('/',(req,res)=>{
 	res.render('home.ejs')
 });
-app.get('/makecampground',async (req,res)=>{
+const validatecamp =(req,res,next)=>{
+	const {error}=joischema.validate(req.body);
+	if(error){
+		throw new AppError(error.message,400);
+	}else{
+		next();
+	}
+}
+app.get('/makecampground', catchAsync(async (req,res)=>{
 	const camp = new  campground({title:'vhjvgfdhjsbfvs', location:'nbvxgdn'});
 	await camp.save();
 	res.send(camp);
-});
+}));
 app.get('/campgrounds/new',(req,res)=>{
 	res.render('campgrounds/new');
 });
-app.post('/campgrounds', async(req,res)=>{
+app.post('/campgrounds',validatecamp, catchAsync(async(req,res)=>{
+	//if(!req.body.campground) throw new AppError('invalid campground',400);
+
 	const campgrounds= new campground(req.body.campground);
 	await campgrounds.save();
-	//console.log(req.params);
-	res.redirect('/campgrounds');
-});
-app.get('/campgrounds/:id', async(req,res)=>{
+//console.log(req.body);
+	res.redirect('/campgrounds/'+campgrounds._id);
+}));
+app.get('/campgrounds/:id', catchAsync(async(req,res)=>{
 	const campgrounds = await campground.findById(req.params.id);
 	//console.log(req.params);
 	res.render('campgrounds/show',{campgrounds});
-});
-app.get('/campgrounds/:id/edit', async(req,res)=>{
+}));
+app.get('/campgrounds/:id/edit', catchAsync(async(req,res)=>{
 	const campgrounds = await campground.findById(req.params.id);
 	//console.log(req.params);
 	res.render('campgrounds/edit',{campgrounds});
-});
-app.get('/campgrounds', async(req,res)=>{
+}));
+app.get('/campgrounds', catchAsync(async(req,res)=>{
 	const campgrounds= await campground.find({});
 	//console.log(campgrounds);
 	res.render('campgrounds/app',{campgrounds});
-} )
-app.put('/campgrounds/:id', async(req,res)=>{
+} ));
+app.put('/campgrounds/:id',validatecamp, catchAsync(async(req,res)=>{
 	const {id}=req.params;
 	const campgrounds=await campground.findByIdAndUpdate(id,{...req.body.campgrounds});
 	console.log(req.body);
 	res.redirect('/campgrounds/'+campgrounds._id);
-});
-app.delete('/campgrounds/:id',async(req,res)=>{
+}));
+app.delete('/campgrounds/:id', catchAsync(async(req,res)=>{
 	//console.log('h');
 	const {id}=req.params;
 	await campground.findByIdAndDelete(id);
 	res.redirect('/campgrounds');
+}));
+app.all('*',(req,res,next)=>{
+	next(new AppError('Page not found',404))
+})
+app.use((err,req,res,next)=>{
+	const {statuscode=500,message="something went wrong"}=err;
+	res.status(statuscode).render('error',{err});
 });
 app.listen(3000,()=>{
 	console.log('serving on port 3000')
---
+++ joischema.js
@@ -0,0 +1,11 @@
+const Joi=require('joi');
+const validator = Joi.object({
+    campground: Joi.object({
+        title: Joi.string().required(),
+        price: Joi.string().required().min(0),
+        image: Joi.string().required(),
+        location: Joi.string().required(),
+        description: Joi.string().required()
+    }).required()
+});
+module.exports=validator;
