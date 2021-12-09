5da1c8dc4ec35ec69312be7d76644ee17979fc2d
prayas7102 <prayas/prithvirajpratap7@gmail.com>
2021-12-10 01:53:33 +0530

    deploy

--- index.js
+++ index.js
@@ -93,6 +93,6 @@ app.use((err,req,res,next)=>{
 	res.status(statuscode).render('error',{err});
 	next();
 });
-app.listen(3000,()=>{
+app.listen(process.env.PORT || 3000,()=>{
 	console.log('serving on port 3000')
 })
